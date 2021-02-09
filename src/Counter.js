72ee3cbe45223412fee2851ab09aac50a1dc3f76
Raul Garcia <punkvomit21@gmail.com>
2021-02-09 00:28:08 -0500

    passed down state using props

--- src/Counter.js
+++ src/Counter.js
@@ -2,26 +2,26 @@ import React from 'react';
 import './App.css';
 
 
-class Counter extends React.Component {
-  state = {
-    name: "Raul garcia"
-  }
+const Name = props => <h1>{props.count}</h1>;
 
+class Counter extends React.Component {
+  state = { count: 0 }
 
   render() {
     return (
       <>
-        <h1 className="count">{this.state.name}</h1>
-        <button className="button" onClick={e => {
-          e.preventDefault();
-          this.setState({
-            name: "Vicious Ways"
-          });
-        }} >Click me!</button>
+        <Name count={this.state.count} />
+        <button className="button"
+          onClick={e => {
+            e.preventDefault();
+            this.setState({
+              count: this.state.count + 1
+            });
+          }}
+        >Click me! + </button>
       </>
     )
-  };
-};
-
+  }
+}
 
 export default Counter;
